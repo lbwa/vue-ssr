5bc8f94dbbcdec45c06db67d4afa832be87e447f
Bowen <mr_lbw@outlook.com>
2018-05-03 22:02:19 +0800

    chore: add new database

--- server/db/db.js
+++ server/db/db.js
@@ -1,13 +1,16 @@
 // request online database
 const sha1 = require('sha1')
+// const md5 = require('md5')
 const axios = require('axios')
 
 // 线上数据库的命名空间
 const className = 'todo'
+// const className = 'todo_list'
 
 // 一个 axios 实例
 const request = axios.create({
   baseURL: 'https://d.apicloud.com/mcm/api'
+  // baseURL: 'https://leancloud.cn/dashboard/apionline/sourcedist/'
 })
 
 // 自定义错误信息
@@ -29,10 +32,17 @@ module.exports = (appId, appKey) => {
   const getHeaders = () => {
     const now = Date.now()
 
+    // apiCloud
     return {
       'X-APICloud-AppId': appId,
       'X-APICloud-AppKey': `${sha1(`${appId}UZ${appKey}UZ${now}`)}.${now}`
     }
+
+    // leanCloud
+    // return {
+    //   'X-LC-Id': appId,
+    //   'X-LC-Sign': `${md5(`${now}${appKey}`)}`
+    // }
   }
 
   return {
